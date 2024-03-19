b4fae2cd2e7c5b47d4c4033722780efff941f360
Михаил Недиков <nedikov.misha@yandex.ru>
2024-03-19 04:48:52 +0300

    [update]: id param, new hook

--- src/components/AuctionItem.jsx
+++ src/components/AuctionItem.jsx
@@ -4,11 +4,16 @@ import { SlBasket } from "react-icons/sl";
 import { MdOutlineFavorite, MdOutlineFavoriteBorder } from "react-icons/md";
 
 import itemPhoto from "./../assets/images/itemPhoto.png";
+import { useTotalOnAuction } from "../hooks/useTotalOnAuction";
 
-function AuctionItem() {
+function AuctionItem({ title, icon, id }) {
   const [isLiked, setIsLiked] = useState(false);
+
+  const total = useTotalOnAuction(id)
+
+
   return (
-    <div className="sm:gap-10 gap-4 flex flex-col lg:w-1/5 md:w-1/4 w-full border-solid border-2 border-red rounded-xl p-5 bg-light">
+    <div className="sm:gap-10 gap-4 flex flex-col lg:w-1/5 md:w-1/4 w-full border-solid border-2 border-red rounded-xl p-5 bg-light transition">
       <div className="flex items-center justify-between">
         <div className="flex flex-col gap-1">
           <div className="flex gap-1">
@@ -27,9 +32,13 @@ function AuctionItem() {
         </div>
       </div>
 
-      <img src={itemPhoto} alt="" className="w-full" />
+      <img
+        src={process.env.REACT_APP_API_GIT_URL + '/ru' +icon}
+        alt=""
+        className="w-1/2   self-center"
+      />
 
-      <p>Golder Hour</p>
+      <p>{title}</p>
     </div>
   );
 }
