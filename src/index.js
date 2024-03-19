1bd3382bf3df24dd580f56025738b09c3cb30089
Михаил Недиков <nedikov.misha@yandex.ru>
2024-03-19 04:46:38 +0300

    [update]: change createrRoot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+
 import "tailwindcss/tailwind.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { store } from "./redux";
+import { Provider } from "react-redux";
+
+const root = createRoot(document.getElementById("root"))
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
