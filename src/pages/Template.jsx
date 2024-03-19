19557dc201b39506211f6070c866f38d1481f1bb
Михаил Недиков <nedikov.misha@yandex.ru>
2024-03-19 04:45:43 +0300

    [update]: auction and template

--- src/pages/Template.jsx
+++ src/pages/Template.jsx
@@ -22,22 +22,24 @@ function Template({ children }) {
 
   return (
     <div
-      className={`relative bg-darkBlue text-white font-inter ${
+      className={`relative flex flex-col justify-between min-h-screen bg-darkBlue text-white font-inter ${
         isOpenModal ? "overflow-hidden h-screen" : ""
       }`}
     >
       <Navbar toggleModal={toggleModal} isOpenModal={isOpenModal} />
       <ModalNavigation isOpen={isOpenModal} />
       {children}
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 50">
-        <path
-          fill="#090A19"
-          fill-opacity="1"
-          d="M0,32L1440,64L1440,320L0,320Z"
-        ></path>
-      </svg>
+      <div className="justify-self-end">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 50">
+          <path
+            fill="#090A19"
+            fillOpacity="1"
+            d="M0,32L1440,64L1440,320L0,320Z"
+          ></path>
+        </svg>
 
-      <Footer />
+        <Footer />
+      </div>
     </div>
   );
 }
