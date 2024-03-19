45d925c7dc3b237c02ef0626bccbd714b9b4371d
Михаил Недиков <nedikov.misha@yandex.ru>
2024-03-19 04:44:44 +0300

    [add]: constants utils

---
+++ src/utils/constants.jsx
@@ -0,0 +1,19 @@
+export const API_URL = {
+  SEARCH: "/listing.json",
+};
+
+export const REGION = {
+  RU: "ru",
+  EU: "eu",
+  NA: "na",
+  SEA: "sea",
+};
+
+export const REGION_URL = {
+  RU: "/ru",
+  EU: "/eu",
+  NA: "/na",
+  SEA: "/sea",
+};
+
+export const PORTION_SIZE = 12;
