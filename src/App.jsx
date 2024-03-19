50df4cc61f0aa0b4cc259afd41bafc9a6695a40d
Михаил Недиков <nedikov.misha@yandex.ru>
2024-03-19 04:49:53 +0300

    [update]: package, App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import Auction from "./pages/Auction";
 
+
 const router = createBrowserRouter([
   {
     path: "/",
