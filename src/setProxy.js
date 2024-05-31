408b556a13765da3cac41858c0153bb45aadfed2
Alexey Bazhenov <alexey931931@gmail.com>
2024-05-31 18:28:24 +0300

    [update]: some fixes

---
+++ src/setProxy.js
@@ -0,0 +1,8 @@
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = function(app) {
+//   app.use('/api', createProxyMiddleware({ 
+//     target: 'https://eapi.stalcraft.net/ru/auction/y1q9/lots', // Здесь указывается URL целевого сервера
+//     changeOrigin: true,
+//   }));
+// };
