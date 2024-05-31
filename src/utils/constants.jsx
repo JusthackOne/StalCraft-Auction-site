82fbe9f8a2fa9a432ef7b1c126f79b7bbce165d3
Михаил Недиков <nedikov.misha@yandex.ru>
2024-05-31 15:53:34 +0300

    [update]: conts for colors

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -16,4 +16,20 @@ export const REGION_URL = {
   SEA: "/sea",
 };
 
+export const NAVBAR_LINK = {
+  MAIN: { to: "/", title: "О проекте" },
+  AUCTION: { to: "/auction", title: "Аукцион" },
+  FAVORITE: { to: "/favorite", title: "Избранное" },
+};
+
+
+export const ITEM_RANK = {
+  RANK_NEWBIE: "rgb(113, 214, 113)",
+  DEFAULT: "rgb(255, 255, 255)",
+  RANK_VETERAN: "rgb(194, 94, 176)",
+  RANK_MASTER: "#E44F5E",
+  RANK_STALKER: "rgb(127, 127, 228)",
+  RANK_LEGEND: "rgb(220, 191, 91)",
+};
+
 export const PORTION_SIZE = 12;
