46d6f6b18a5bb7efe1b84f78a8584dd4ca74e500
Alexey Bazhenov <alexey931931@gmail.com>
2024-05-31 18:25:34 +0300

    [add]: route for auction item page

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,8 +1,10 @@
+import AuctionItem from "../pages/AuctionItem";
 import Auction from "../pages/Auction";
 import MainPage from "../pages/MainPage";
 
 export const ROUTES = {
     MAIN: { to: "/", component: <MainPage /> },
     AUCTION: { to: "/auction", component: <Auction /> },
+    AUCTION_ITEM: { to: "/auction/:id", component: <AuctionItem /> },
     FAVORITE: { to: "/favorite", component: <MainPage /> },
   };
